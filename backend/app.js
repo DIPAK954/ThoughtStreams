383f618d578cb37defc0388b18b14f5597a779c8
Dipak <165145961+DIPAK954@users.noreply.github.com>
2025-08-06 23:01:16 +0530

    Update app.js Frontend URL

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://thoughtstreams-frontend.onrender.com',
+  origin: 'https://thoughtstream-bbax.onrender.com',
   credentials: true
 }));
 
